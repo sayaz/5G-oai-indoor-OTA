b60ef18d3f2ac93c511b943849ee1118a6d2086c
Syed Ayaz Mahmud <sayazmahmud@gmail.com>
2023-01-14 17:54:57 -0700

    Update

--- bin/deploy-oai.sh
+++ bin/deploy-oai.sh
@@ -108,8 +108,8 @@ function setup_cn_node {
     echo replacing a couple of configuration files
     cp /local/repository/etc/oai/docker-compose-basic-nrf.yaml /var/tmp/oai-cn5g-fed/docker-compose/docker-compose-basic-nrf.yaml
     cp /local/repository/etc/oai/docker-compose-mini-nrf.yaml /var/tmp/oai-cn5g-fed/docker-compose/docker-compose-mini-nrf.yaml
-    cp /local/repository/etc/oai/oai_db1.sql /var/tmp/oai-cn5g-fed/docker-compose/oai_db1.sql
-    cp /local/repository/etc/oai/oai_db2.sql /var/tmp/oai-cn5g-fed/docker-compose/oai_db2.sql
+    #cp /local/repository/etc/oai/oai_db1.sql /var/tmp/oai-cn5g-fed/docker-compose/oai_db1.sql
+    #cp /local/repository/etc/oai/oai_db2.sql /var/tmp/oai-cn5g-fed/docker-compose/oai_db2.sql
     echo setting up cn node... done.
 
 }
